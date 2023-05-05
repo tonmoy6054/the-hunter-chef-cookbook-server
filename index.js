f41fc30d1ca2bf0998ecdbce3db6d23841850525
Tonmoy Ghosh <tonmoy.mec@gmail.com>
2023-05-05 16:17:27 +0600

    recipe.json made

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
-
+app.use(cors());
 app.get('/', (req, res) => {
     res.send('Food item is coming')
 
 })
 
 app.get('/categories', (req, res) => {
-    res.send(categories)
+    res.send(categories);
 })
+
+app.get('/catagories/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectedData = categories.find(n => n.id === id);
+    res.send(selectedData);
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Food item is running on port: ${port}`);
